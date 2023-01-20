79a5bd0a655258b9688a7b5d9de6a543cae10abd
Caleb McAnuff <cjmcanuff@yahoo.ca>
2023-01-20 00:13:31 -0500

    basic template

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tesla</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Me</a></li>
+          <li><a href='#'>Contact Me</a></li>
+          <li><a href='#'>Inventory</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
